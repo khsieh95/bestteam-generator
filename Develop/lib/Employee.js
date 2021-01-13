2e0d26a4bf3f7a78534f533ff830133e9d887fa5
Kevin Hsieh <khsieh95@gmail.com>
2021-01-12 16:17:16 -0800

    installed dependencies

---
+++ Develop/lib/Employee.js
@@ -0,0 +1,9 @@
+// TODO: Write code to define and export the Employee class
+
+class Employee {
+  constructor(id, email, github) {
+    this.id = id;
+    this.email = email;
+    this.github = github;
+  }
+}
