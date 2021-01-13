4873a1fc2bea53682da997b636140def3685b549
Kevin Hsieh <khsieh95@gmail.com>
2021-01-12 17:38:24 -0800

    passed first suite

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -5,12 +5,19 @@ class Employee {
     this.id = id;
     this.name = name;
     this.email = email;
+    this.role = "Employee";
   }
   getName() {
     return this.name;
   }
   getId() {
     return this.id;
+  }
+  getEmail() {
+    return this.email;
+  }
+  getRole() {
+    return this.role;
   }
 }
 
